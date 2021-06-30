81b0fc44d1b0a7c6fde466f3ec19047b37fb84e6
Knowledgebase <71754369+knowledgebaseit@users.noreply.github.com>
2021-06-30 10:30:58 +0200

    Update Download_NL_Win10_LXP.ps1

--- Customize/Languagepack/Download_NL_Win10_LXP.ps1
+++ Customize/Languagepack/Download_NL_Win10_LXP.ps1
@@ -33,5 +33,3 @@ Set-Location -Path C:\\Temp\\Languagepack
 write-host 'AIB Customization: Install NL Languagepack '
 .\Install_NL_Win10_LXP.ps1
 write-host "Installation Install NL Languagepack Complete"
-write-host 'AIB Customization: Restart Computer '
-Restart-Computer -Force
