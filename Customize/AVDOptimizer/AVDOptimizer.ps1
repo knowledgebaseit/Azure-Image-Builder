af24538b9fe3e34ad7db2636e7e2b46179c6cc81
Knowledgebase <71754369+knowledgebaseit@users.noreply.github.com>
2021-07-06 10:25:43 +0200

    Update AVDOptimizer.ps1

--- Customize/AVDOptimizer/AVDOptimizer.ps1
+++ Customize/AVDOptimizer/AVDOptimizer.ps1
@@ -1,46 +1,38 @@
-# OS Optimizations for WVD
-write-host 'AIB Customization: OS Optimizations for WVD'
-$appName = 'Optimize'
-$drive = 'C:\Temp'
-New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
-$LocalPath = $drive + '\' + $appName 
-set-Location $LocalPath
-$osOptURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/refs/heads/main.zip'
-$osOptURLexe = 'Windows_10_VDI_Optimize-master.zip'
-$outputPath = $LocalPath + '\' + $osOptURLexe
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri $osOptURL -OutFile $outputPath
-write-host 'AIB Customization: Starting OS Optimizations script'
-Expand-Archive -LiteralPath 'C:\Temp\Optimize\Windows_10_VDI_Optimize-master.zip' -DestinationPath $Localpath -Force -Verbose
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
-Set-Location -Path C:\\Temp\\Optimize\\Virtual-Desktop-Optimization-Tool-main
+##############################################
+#    WVD Optimizer (Virtual Desktop Team)    #
+##############################################
+    ################################
+    #    Download WVD Optimizer    #
+    ################################
+    New-Item -Path C:\ -Name Optimize -ItemType Directory -ErrorAction SilentlyContinue
+    $LocalPath = "C:\Optimize\"
+    $WVDOptimizeURL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/refs/heads/main.zip'
+    $WVDOptimizeInstaller = "Windows_10_VDI_Optimize-master.zip"
+    $ProgressPreference = 'SilentlyContinue'
+    Invoke-WebRequest `
+        -Uri $WVDOptimizeURL `
+        -OutFile "$Localpath$WVDOptimizeInstaller"
 
-# instrumentation
-$osOptURL = 'https://raw.githubusercontent.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/master/Win10_VirtualDesktop_Optimize.ps1'
-$osOptURLexe = 'optimize.ps1'
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri $osOptURL -OutFile $osOptURLexe
 
-# Patch: overide the Win10_VirtualDesktop_Optimize.ps1 - setting 'Set-NetAdapterAdvancedProperty'(see readme.md)
-Write-Host 'Patch: Disabling Set-NetAdapterAdvancedProperty'
-$updatePath= "C:\Temp\Optimize\Virtual-Desktop-Optimization-Tool-main\Win10_VirtualDesktop_Optimize.ps1"
-((Get-Content -path $updatePath -Raw) -replace 'Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB','#Set-NetAdapterAdvancedProperty -DisplayName "Send Buffer Size" -DisplayValue 4MB') | Set-Content -Path $updatePath
+    ###############################
+    #    Prep for WVD Optimize    #
+    ###############################
+    Expand-Archive `
+        -LiteralPath "C:\Optimize\Windows_10_VDI_Optimize-master.zip" `
+        -DestinationPath "$Localpath" `
+        -Force `
+        -Verbose
 
-# Patch: overide the REG UNLOAD, needs GC before, otherwise will Access Deny unload(see readme.md)
 
-[System.Collections.ArrayList]$file = Get-Content $updatePath
-$insert = @()
-for ($i=0; $i -lt $file.count; $i++) {
-  if ($file[$i] -like "*& REG UNLOAD HKLM\DEFAULT*") {
-    $insert += $i-1 
-  }
-}
 
-#add gc and sleep
-$insert | ForEach-Object { $file.insert($_,"                 Write-Host 'Patch closing handles and runnng GC before reg unload' `n              `$newKey.Handle.close()` `n              [gc]::collect() `n                Start-Sleep -Seconds 15 ") }
-Set-Content $updatePath $file
+    #################################
+    #    Run WVD Optimize Script    #
+    #################################
+    Set-Location -Path C:\\Optimize\\Virtual-Desktop-Optimization-Tool-main
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force -Verbose
+    .\Win10_VirtualDesktop_Optimize.ps1 -Optimizations All -Restart -AcceptEULA -Verbose
 
-# run script
-# .\optimize -WindowsVersion 2004 -Verbose
-.\Win10_VirtualDesktop_Optimize.ps1 -Optimizations All -Restart -AcceptEULA -Verbose
- write-host 'AIB Customization: Finished OS Optimizations script'
+##########################
+#    Restart Computer    #
+##########################
+Restart-Computer -Force 
